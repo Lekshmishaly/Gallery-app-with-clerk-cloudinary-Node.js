4e8602d7a55af497653fa2cdafc92e93f5f01eb3
Lekshmi Shaly <lekshmishaly@Lekshmis-MacBook-Air.local>
2025-07-20 21:37:57 +0530

    added user schema

---
+++ models/userModel.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema({
+  clerkUserId: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  email: {
+    type: String,
+    required: true,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+  },
+});
+
+module.exports = mongoose.model("User", userSchema);
